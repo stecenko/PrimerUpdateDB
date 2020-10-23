c0655aace161c7a6c1ff5a8d2fb0a4b03c00b8da
Александр <stecenko@gmail.com>
2020-10-23 12:45:59 +0300

    Пример: при update не обновляется таблица во вьюхе.

---
+++ PrimerUpdate/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using PrimerUpdate.Model;
+using System.Data.Entity;
+using System.Windows;
+
+namespace PrimerUpdate
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PrimerRepository primerRepository;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            primerRepository = new PrimerRepository();
+
+            GridTovar.ItemsSource = primerRepository.GetTovars();
+            GridViewTovar.ItemsSource = primerRepository.GetViewTovars();
+        }
+
+
+        private void AddTovar_Click(object sender, RoutedEventArgs e)
+        {
+            primerRepository.InsertTovar(tbName.Text);
+            GridTovar.ItemsSource = null;
+            GridTovar.ItemsSource = primerRepository.GetTovars();
+
+            GridViewTovar.ItemsSource = null;
+            GridViewTovar.ItemsSource = primerRepository.GetViewTovars();
+        }
+
+        private void UpdateTovar_Click(object sender, RoutedEventArgs e)
+        {
+            Tovar tovar = (Tovar)GridTovar.SelectedItem;
+            //tovar.IdTovar; tbName
+            primerRepository.UpdateTovar(tovar.IdTovar, tbName.Text);
+
+            GridTovar.ItemsSource = null;
+            GridTovar.ItemsSource = primerRepository.GetTovars();
+
+            GridViewTovar.ItemsSource = null;
+            GridViewTovar.ItemsSource = primerRepository.GetViewTovars();
+            
+        }
+    }
+}
---
+++ PrimerUpdate/Model/PrimerRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerUpdate.Model
+{
+    public class PrimerRepository: INotifyPropertyChanged
+    {
+        PrimerEntities primerEntities = new PrimerEntities();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public IEnumerable<ViewTovar> GetViewTovars()
+        {
+            return primerEntities.ViewTovars.ToList();
+        }
+
+
+        public IEnumerable<Tovar> GetTovars()
+        {
+            return primerEntities.Tovars.ToList();
+        }
+
+
+
+        public void InsertTovar(string nameTovar)
+        {
+            // вставляем товар
+            var tovar = primerEntities.Tovars.Add(new Tovar() { NameTorar = nameTovar, TypeTovar = 1 });
+            primerEntities.SaveChanges();
+
+            int id = tovar.IdTovar; // получаем ID только после сохранения данных SaveChanges
+
+            var price = primerEntities.Prices.Add(new Price() { idTovar = id, PriceTovar = 100 });
+            primerEntities.SaveChanges();
+        }
+
+        public void UpdateTovar(int idTovar, string nameTovar)
+        {
+            var tovar = primerEntities.Tovars.Where(a => a.IdTovar == idTovar).FirstOrDefault();
+            tovar.NameTorar = nameTovar;
+
+            primerEntities.Entry(tovar).State = System.Data.Entity.EntityState.Modified;
+            primerEntities.SaveChanges();
+        }
+    }
+}
